e34b85154e8aa191b77f2ec72f522de4548f594b
geoff callaghan <irishgeoff@yahoo.com>
2025-09-15 14:58:40 +0100

    working

--- scraper-and-emailer/send-emails.js
+++ scraper-and-emailer/send-emails.js
@@ -1,4 +1,24 @@
 const sqlite3 = require("sqlite3").verbose();
+import { sendEmail } from './mailer.js';
+import { qrFeedbackTemplate } from './templates/qrFeedback.js';
+import { renderTemplate } from './utils/renderTemplate.js';
+
+async function SendEmailToCoffeeShop(toEmail,shopName) {
+  const filled = renderTemplate(qrFeedbackTemplate, {
+    shopName: shopName,
+    guestType: "coffee shop guests",
+  });
+
+  await sendEmail({
+    toEmail: toEMail,
+    toName: toName,
+    subject: filled.subject,
+    textBody: filled.textBody,
+    htmlBody: filled.htmlBody,
+  });
+}
+
+;
 
 // Open database
 const db = new sqlite3.Database("./emails.db");
@@ -15,10 +35,12 @@ function processEmails() {
         }
 
         console.log(`Sending email to: ${row.email} (Shop: ${row.shop_name})`);
-
+        sendEmailToCoffeeShop(${row.email},${row.shopName});
         try {
           // ---- Your real email-sending logic goes here ----
-          await fakeSendEmail(row.email);
+          // await fakeSendEmail(row.email);
+
+          await sendEmailToCoffeeShop(${row.email},${row.shopName});
 
           // Mark as sent
           db.run(
